b6bc5c35d322ef6e9f8c5410e782cc678a6cb602
Daniel Abbott <danielabbott@daniel-abbott.com>
2019-07-02 21:03:53 -0600

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Nav from './Nav.svelte';
+	export let name;
+</script>
+
+<style>
+	.app {
+		display: flex;
+	}
+	h1 {
+		color: purple;
+	}
+</style>
+
+<div class="app">
+	<Nav />
+	<h1>Hello {name}!</h1>
+</div>
+
---
+++ src/Nav.svelte
@@ -0,0 +1,13 @@
+<script>
+    
+</script>
+
+<style>
+    .nav {
+        background-color: green;
+    }
+</style>
+    
+<div class="nav">
+    <h1>Portfolio</h1>
+</div>
